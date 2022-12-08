a8f1a4d232e2ed992a734cbc90de3ae932d8795d
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-12-08 13:16:39 -0300

    Summary component styles done

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
