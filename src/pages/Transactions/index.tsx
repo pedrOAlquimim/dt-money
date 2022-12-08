c5a2304b9830518878624d6a14b99ffeb318ad1d
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-12-08 14:38:30 -0300

    Transaction table styles done

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  TransactionContainer,
+  TransactionTable,
+  PriceHighlight
+} from './styles'
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburguer</td>
+              <td>
+                <PriceHighlight variant='outcome'>
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
