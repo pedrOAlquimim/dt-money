b4d4a323280fff94662eed2ccf04fe1ed38c8d36
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-12-08 11:26:34 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>Hello world</div>
+    </ThemeProvider>
+  )
+}
+
